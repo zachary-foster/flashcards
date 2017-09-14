702c41b93a0e9ebb6a0b0eb405551d39c7583557
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2017-09-14 16:05:46 -0700

    trying to add travis and codecov

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(flashcards)
+
+test_check("flashcards")
---
+++ tests/testthat/test_init.R
@@ -0,0 +1,6 @@
+library(flashcards)
+context("Initalizing files/folders")
+
+test_that("Test test", {
+  expect_equal(1, 1)
+})
