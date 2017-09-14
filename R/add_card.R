e57daeea01f480178f1dd48a470cdc171ef41e3c
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2017-09-14 12:57:51 -0700

    card adding working

--- R/add_card.R
+++ R/add_card.R
@@ -21,7 +21,6 @@
 add_card <- function(deck_path, front, back, difficulty = 1, source = "",
                      source_url = "", rename_img = TRUE, max_pixels = 300000) {
   # Check if the front or back of the cards are images
-  image_formats <- c("jpg", "jpeg", "png", "bmp")
   process_if_image <- function(path, other_side) {
     if (is_image_path(path)) {
       if (! is_image_path(other_side) && rename_img) {
@@ -29,11 +28,24 @@ add_card <- function(deck_path, front, back, difficulty = 1, source = "",
       } else {
         img_name <- basename(path)
       }
-      add_image(path, deck_path, img_name, max_pixels = max_pixels)
+      output <- basename(add_image(img_path = path, deck_path = deck_path,
+                                   name = img_name, max_pixels = max_pixels))
+    } else {
+      output <- path
+    }
+    return(output)
+  }
+  front <- process_if_image(front, back)
+  back <- process_if_image(back, front)
+
+  # If source_url is not specified and the front or back is a url, set source_url
+  if (missing(source_url) && source_url == "") {
+    if (RCurl::url.exists(front)) {
+      source_url <- front
+    } else if (RCurl::url.exists(back)) {
+      source_url <- back
     }
   }
-  process_if_image(front, back)
-  process_if_image(back, front)
 
   # Create card data row
   output <- data.frame(front = front, back = back, difficulty = difficulty,
@@ -41,6 +53,13 @@ add_card <- function(deck_path, front, back, difficulty = 1, source = "",
 
   # Add cards to deck
   tsv_path <- file.path(deck_path, "deck.tsv")
+  deck_data <- read.table(tsv_path, header = TRUE, sep = "\t", fill = TRUE, stringsAsFactors = FALSE)
+  new_deck <- rbind(deck_data, output)
+
+  # Save deck data
+  write.table(new_deck, file = tsv_path, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+
+  return(NULL)
 }
 
 #' Check if a string is a image path
@@ -51,6 +70,7 @@ add_card <- function(deck_path, front, back, difficulty = 1, source = "",
 #'
 #' @keywords internal
 is_image_path <- function(path) {
+  image_formats <- c("jpg", "jpeg", "png", "bmp")
   tolower(tools::file_ext(path)) %in% image_formats && (file.exists(path) || RCurl::url.exists(path))
 }
 
@@ -64,13 +84,15 @@ is_image_path <- function(path) {
 #' @param deck_path The path to the deck directory
 #' @param max_pixels The maximum number of pixels an image can be. Larger images
 #'   will be shrunk to this size and saved as a jpeg.
+#'
+#' @return The path to where the image was saved
+#'
 add_image <- function(img_path, name = basename(img_path), deck_path, max_pixels = 300000) {
   # Internal params
   max_char <- 100 # The maximum number of characters in a file name
 
-
   # Sanitize file name
-  # name <- paste0(gsub(pattern = "[\/:*?<>|' ]", replacement = "_", tolower(other_side)))
+  name <- paste0(gsub(pattern = "[\\/:*?<>|' ]", replacement = "_", tolower(name)))
 
   # Apply length limit to file name
   if (nchar(name) > max_char) {
@@ -83,12 +105,14 @@ add_image <- function(img_path, name = basename(img_path), deck_path, max_pixels
 
   # Make image destination path unique if not already
   if (file.exists(img_dest_path)) {
-    name_suffix <- stringr::str_match(tools::file_path_sans_ext(name), ".*_([0-9]+)$")[,2]
-    if (is.na(name_suffix)) {
-      name_suffix <- 1
+    all_suffixes <- as.numeric(stringr::str_match(list.files(img_dir_path), ".*_([0-9]+)\\..+$")[,2])
+    if (all(is.na(all_suffixes))) {
+      name_suffix <- 2
+    } else {
+      name_suffix <- max(all_suffixes, na.rm = TRUE) + 1
     }
     img_dest_path <- paste0(tools::file_path_sans_ext(img_dest_path),
-                            "_", name_suffix, tools::file_ext(img_dest_path))
+                            "_", name_suffix, ".", tools::file_ext(img_dest_path))
   }
 
   # Load image
@@ -99,8 +123,12 @@ add_image <- function(img_path, name = basename(img_path), deck_path, max_pixels
     width <- round(sqrt((dim(img)[1] * max_pixels) /  dim(img)[2]))
     height <- round(sqrt((dim(img)[2] * max_pixels) /  dim(img)[1]))
     img <- imager::resize(img, size_x = width, size_y = height)
+    my_print("Reducing image size to ", height, " by ", width, ".")
   }
 
   # Save image
   imager::save.image(img, img_dest_path)
+
+  # Return the path where the image was save
+  return(img_dest_path)
 }
